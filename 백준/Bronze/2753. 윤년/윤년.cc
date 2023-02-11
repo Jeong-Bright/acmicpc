ad256cea08db248c270a14d761d47e8ea6c9da15
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-02-12 02:22:24 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int tf(int a){
+	if(a % 4 == 0 && (a % 100 != 0 || a % 400 == 0))
+	return true;
+	else
+	return false;
+}
+
+int main() {
+	
+	int y;
+	cin >> y;
+	cout << tf(y);	
+	
+	
+}
