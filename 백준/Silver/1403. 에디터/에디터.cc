39c48533cdbec0a874966ea879b8e68396a5d08d
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-03-24 09:40:10 +0900

    Update 에디터.cc

--- 백준/Silver/1403. 에디터/에디터.cc
+++ 백준/Silver/1403. 에디터/에디터.cc
@@ -1,71 +1,49 @@
 #include <bits/stdc++.h>
 #define call ios::sync_with_stdio(0); cin.tie(0);
-#define MX 2000005
 using namespace std;
-int dat[MX];
-int tail = 0; int head = 0;
-
-void push(int x) {
-	dat[tail++] = x;
-}
-
-int pop() {
-	if (tail==head) return -1;
-	else {
-		return dat[head++];
-	}
-}
-
-int empty() {
-	if (tail == head) return 1;
-	else return 0;
-
-}
-
-int front() {
-	if (empty()) return -1;
-	return dat[head];
-}
-
-int back() {
-	if (empty()) return -1;
-	return dat[tail - 1];
-}
-
-
-void size() {
-	if (tail - head > 0)
-		cout << tail - head << '\n';
-	else cout << 0 << '\n';
-}
 
 int main() {
 
 	call;
-	int a,N;
-	string z;
+	int A;
+	char c,k; string N;
+	list<char> L;
+	list<char>::iterator cur;
 	cin >> N;
-	for (int i = 0; i < N;i++) {
-		cin >> z;
-		if (z == "push"){
-			cin >> a;
-			push(a);		
+	cin >> A;
+	for (int i = 0;i < N.length() ;i++) {
+		L.push_back(N[i]);
+	}
+	cur = L.end();
+
+	for (int i = 0; i < A; i++) {
+		cin >> c;
+		if (c == 'L') {
+			if (cur == L.begin()) continue;
+			cur--;
 		}
-		if (z == "pop") {
-			cout << pop() << '\n';
+		if (c == 'D') {
+			if (cur == L.end()) continue;
+			cur++;
+
 		}
-		if (z == "size") {
-			size();
+		if (c == 'B') {
+			if (cur == L.begin()) continue;
+			cur = L.erase(--cur);
 		}
-		if (z == "empty") {
-			cout << empty() << '\n';
-		}
-		if (z == "front") {
-			cout << front() << '\n';
-		}
-		if (z == "back") {
-			cout << back() << '\n';
+		if (c == 'P') {
+			cin >> k;
+			L.insert(cur, k);
 		}
 
 	}
+
+	for (auto z : L) {
+		cout << z;
+	}
+
+
+	
+	
+
 }
