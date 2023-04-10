39b3124fc9ff52b230abd3b601be4862f16b9890
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-04-10 17:08:13 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define call ios_base::sync_with_stdio(0); cin.tie(0);
+#define MX 2000005
+#define MAX 0x7ffffff
+#define ll long long
+#define endl '\n';
+using namespace std;
+int n, m;
+int arr[10];
+int a[10];
+bool isused[10];
+
+
+void func(int k) { 
+    if (k == m) {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' '; 
+        cout << '\n';
+        return;
+    }
+
+    int x = 0;
+    for (int i = 0; i < n; i++) {
+        if(!isused[i] && a[i] != x) {
+            arr[k] = a[i];
+            x = arr[k];
+            isused[i] = 1;
+            func(k + 1);
+            isused[i] = 0; 
+        }
+        }
+ }
+ 
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+
+    }
+    sort(a, a + n);
+    func(0);
+}
