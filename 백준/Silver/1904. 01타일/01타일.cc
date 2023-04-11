54a5214bb665c8b7cabab3a7d7da945d8cbd9490
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-04-11 14:11:12 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 87960 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define call ios_base::sync_with_stdio(0); cin.tie(0);
+#define MX 2000005
+#define MAX 0x7ffffff
+#define ll long long
+#define endl '\n'
+using namespace std;
+vector<int> v;
+stack<long long> s;
+ll a[1000005], b[10000005];
+
+int main(void) {
+    call;
+    int N;
+    cin >> N;
+    a[1] = 1; a[2] = 2;
+    for (int i = 3; i <= N; i++) {
+        a[i] = (a[i - 1] + a[i - 2])%15746;
+    }
+
+    if (N < 1) cout << -1;
+    else
+    cout << a[N];
+    
+
+}
