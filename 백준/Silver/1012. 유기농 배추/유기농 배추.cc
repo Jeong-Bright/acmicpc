7c259fe57be861a74ed58c1272bfc55abdb9e424
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-05-02 13:18:29 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 3256 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define call ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MX 2000005
+#define MAX 0x7ffffff
+#define endl '\n'
+#define X first
+#define Y second
+int board[502][502];
+bool vis[502][502];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int n, m, k;
+queue <pair<int, int>> Q;
+
+void bfs(int x, int y) {
+    vis[x][y] = true;
+    Q.push({ x,y });
+    while (!Q.empty()) {
+        auto cur = Q.front(); Q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (vis[nx][ny] || board[nx][ny] != 1) continue;
+            vis[nx][ny] = true;
+            Q.push({ nx, ny });
+        }
+    }
+}
+
+int main(void) {
+    call;
+    int t; cin >> t;
+    while (t--) {
+        cin >> m >> n >> k;
+        int x, y;
+        for (int i = 0; i < k; i++) {
+            cin >> x >> y;
+            board[y][x] = 1;
+        }
+        int area = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1 && !vis[i][j]) {
+                    bfs(i, j);
+                    area++;
+
+                }
+            }
+        }
+
+        cout << area << endl;
+        for (int i = 0; i < n; i++) {
+            fill(board[i], board[i] + m, 0);
+            fill(vis[i], vis[i] + m, false);
+        }
+    }
+    return 0;
+}
