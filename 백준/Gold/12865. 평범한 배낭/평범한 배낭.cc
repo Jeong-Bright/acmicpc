76b1f2406a97b51b54e909e8b6940ddebb4bc20f
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-05-07 15:16:42 +0900

    Update 평범한 배낭.cc

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,41 +1,32 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
- 
-int w[100001];
-int v[100001];
-int dp[101][100001];
- 
- 
-int main(void) {
-    cout.tie(NULL);
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    int N, K;
-    int result = 0;
- 
-    cin >> N >> K;
- 
-    for (int i = 1; i <= N; i++) {
-        cin >> w[i];
-        cin >> v[i];
-    }
- 
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= K; j++) {
-            //현재 물건 안넣고 이전 값 vs 
-            //현재 물건 넣기 위해 현재 물건의 무게만큼 빼주고(그래야 무게 초과 안되고 자리가 난다) 이전 값의 최댓값을 넣으면서 현재 가치 더함.
-            //둘 중 큰 값을 넣어준다.
-            if (w[i]<=j) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
-            
-            //무게가 배낭보다 크면 그냥 복사해준다.
-            else dp[i][j] = dp[i - 1][j];
-        }
-    }
- 
- 
-    cout << dp[N][K];
- 
-    return 0;
+#define call ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MX 2000005
+#define MAX 0x7ffffff
+#define endl '\n'
+typedef long long ll;
+int cnt[MX];
+int weight[MX];
+int dp[1005][100005];
+int A[MX]; 
+int B[MX];
+vector<int> v, v1;
+int main() {
+	call;
+	int T,X; cin >> T >> X;
+	for (int i = 1; i <= T; i++) {
+		cin >> cnt[i] >> weight[i];
+	}
+
+	for (int i = 1; i <= T;i++) {
+		for (int j = 1; j <= X; j++) {
+			if (cnt[i] <= j) {
+				dp[i][j] = max(dp[i - 1][j], dp[i-1][j - cnt[i]] + weight[i]);
+			}
+			else
+				dp[i][j] = dp[i - 1][j];	
+
+		}
+	}
+	cout << dp[T][X];
 }
