8ddd3febb768d9632d07437a78a3da0ee98c4957
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-08-10 14:23:20 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 5284 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,28 +1,32 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+#define endl '\n';
+#define call ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+vector<int> v;
+vector<int> adj[100005];
+int p[100005];
+
+
 
 int main() {
-
-	stack<int> st;
-
-	int sum = 0;
-	int a;
-	int n; cin >> n;
-	for (int i = 0;i < n;i++) {
-		int c; cin >> c;
-	
-		if (c == 0) {
-			a = st.top();
-			st.pop();
-			sum -= a;
-			
-		}
-		else {
-			st.push(c);
-			sum += st.top();
-		}
-	}
-	cout << sum;
+    call;
+    stack<int> stk;
+    int v; cin >> v;
+    int sum = 0;
+    while (v--) {
+        int b; cin >> b;
+        if (b == 0) {
+            if (!stk.empty()) {
+                sum -= stk.top();
+                stk.pop();
+            }
+        }
+        else {
+            stk.push(b); sum += stk.top();
+        }
+    }
+    cout << sum;
+ 
 
 }
