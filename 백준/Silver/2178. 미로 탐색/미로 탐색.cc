2e16c2760536d32f24b09dd9bc3f4f60fe374d66
jeongbright <104780993+Jeong-Bright@users.noreply.github.com>
2023-08-11 10:34:29 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 4936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,32 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-string board[102];
-int dist[102][102];
-int n,m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  for(int i = 0; i < n; i++)
-    cin >> board[i];
-  for(int i = 0; i < n; i++) fill(dist[i],dist[i]+m,-1);
-  queue<pair<int,int> > Q;
-  Q.push({0,0});
-  dist[0][0] = 0;
-  while(!Q.empty()){
-    auto cur = Q.front(); Q.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
-      dist[nx][ny] = dist[cur.X][cur.Y]+1;
-      Q.push({nx,ny});
-    }
-  }
-  cout << dist[n-1][m-1]+1; // 문제의 특성상 거리+1이 정답
+typedef long long ll;
+#define endl '\n'
+#define call ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+vector<int> v;
+vector<int> adj[100005];
+int p[100005];
+int T, X;
+string board[105];
+int dist[105][105];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+int cnt = 0, area = 0, num = 0;
+void bfs() {
+          for(int i = 0 ; i < T ;i++)  fill(dist[i], dist[i] + X, -1);
+            queue<pair<int, int>> q;
+            q.push({ 0,0 });
+            dist[0][0] = 0;
+            while (!q.empty()) {
+                cnt++;
+                pair<int, int> cur = q.front();
+                q.pop();
+                for (int dir = 0; dir < 4; dir++) {
+                    int nx = dx[dir] + cur.first;
+                    int ny = dy[dir] + cur.second;
+                    if (nx >= T || nx < 0 || ny >= X || ny < 0) continue;
+                    if (dist[nx][ny] >= 0 || board[nx][ny] != '1') continue;
+                    q.push({ nx, ny });
+                    dist[nx][ny] = dist[cur.first][cur.second] + 1;
+                }
+            }
+}
+
+
+int main() {
+    call;
+    cin >> T >> X;
+    for (int i = 0; i < T; i++)
+            cin >> board[i];
+    
+    bfs();
+    cout << dist[T - 1][X - 1] + 1;
+
 }
